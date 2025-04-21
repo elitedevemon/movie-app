c78548a79a7d8bd92d0483baf63252f671823c26
EliteDev Emon <elitedevemon@gmail.com>
2025-04-21 21:12:24 +0600

    auth functions

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TMDBController;
 use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
@@ -17,19 +18,21 @@ Route::get('/dashboard', function () {
 
 Route::get('tmdb', [TMDBController::class, 'index'])->name('tmdb');
 
-Route::middleware('auth')->group(function () {
-  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 
 require __DIR__ . '/auth.php';
 
 //admin routes
-Route::prefix('admin')->name('admin.')->group(function () {
+Route::prefix('admin')->middleware(['auth', 'verified', 'throttle'])->name('admin.')->group(function () {
   Route::prefix('dashboard')->group(function(){
     Route::get('/', [AdminController::class, 'index'])->name('dashboard');
     Route::resource('/videos', VideoController::class);
   });
 });
+
+Auth::routes([
+  'register' => false,
+  'reset' => false,
+  'verify' => false,
+]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
